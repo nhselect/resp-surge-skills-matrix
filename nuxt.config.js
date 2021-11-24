47c2feee8dffc76b8b4c3ca3da7d49e3c144ed1e
timberrr <timberrr@users.noreply.github.com>
2021-11-24 12:33:38 +0000

    removed buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   // modules: ['nuxt-buefy', '@nuxt/content'],
-  modules: ['nuxt-buefy', '@nuxt/content'],
+  modules: ['@nuxt/content'],
 
   content: {},
 
